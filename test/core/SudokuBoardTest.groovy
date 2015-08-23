be6e36420d56071610f364074675ee64bdcd1027
Jerran Simpson <jjsimpson889@gmail.com>
2015-08-23 15:08:58 -0500

    Finished writing basic unit tests for calculating possible values and possible value occurrences

--- test/core/SudokuBoardTest.groovy
+++ test/core/SudokuBoardTest.groovy
@@ -99,97 +99,97 @@ class SudokuBoardTest extends GroovyTestCase {
         Set[][] result = new Set[9][9]
         result[0][0] = [1,6,7,8,9] as Set
         result[0][1] = [1,4,5,6,7,9] as Set
-        result[0][3] = [1,3,6,7,9] as Set
+        result[0][2] = [] as Set
+        result[0][3] = [1,6,7,9] as Set
         result[0][4] = [4,5,6,7,8,9] as Set
+        result[0][5] = [] as Set
         result[0][6] = [1,5,7,8] as Set
         result[0][7] = [1,4,5,6,8,9] as Set
         result[0][8] = [5,7,8,9] as Set
 
         result[1][0] = [2,8,9] as Set
         result[1][1] = [2,3,5,9] as Set
+        result[1][2] = [] as Set
         result[1][3] = [2,3,9] as Set
+        result[1][4] = [] as Set
         result[1][5] = [3,5,8,9] as Set
         result[1][6] = [5,8] as Set
+        result[1][7] = [] as Set
+        result[1][8] = [] as Set
 
         result[2][0] = [2,6,8,9] as Set
-        result[2][1] = [2,3,4,5,6,9] as Set
+        result[2][1] = [2,3,5,6,9] as Set
+        result[2][2] = [] as Set
         result[2][3] = [2,3,6,9] as Set
         result[2][4] = [3,5,6,8,9] as Set
+        result[2][5] = [] as Set
+        result[2][6] = [] as Set
         result[2][7] = [2,3,5,6,8,9] as Set
         result[2][8] = [2,3,5,8,9] as Set
 
+        result[3][0] = [] as Set
         result[3][1] = [2,3,5,7,9] as Set
         result[3][2] = [5,8,9] as Set
         result[3][3] = [2,3,7,9] as Set
         result[3][4] = [3,5,7,8,9] as Set
         result[3][5] = [3,5,8,9] as Set
+        result[3][6] = [] as Set
         result[3][7] = [2,3,5,8,9] as Set
+        result[3][8] = [] as Set
 
-        result[4][0] = [] as Set
-        result[4][1] = [] as Set
-        result[4][2] = [] as Set
+        result[4][0] = [1,2,6,7,8,9] as Set
+        result[4][1] = [1,2,3,6,7,9] as Set
+        result[4][2] = [6,8,9] as Set
         result[4][3] = [] as Set
-        result[4][4] = [] as Set
+        result[4][4] = [3,6,7,8,9] as Set
         result[4][5] = [] as Set
-        result[4][6] = [] as Set
-        result[4][7] = [] as Set
-        result[4][8] = [] as Set
+        result[4][6] = [1,7,8] as Set
+        result[4][7] = [1,2,3,6,8,9] as Set
+        result[4][8] = [2,3,7,8,9] as Set
 
         result[5][0] = [] as Set
-        result[5][1] = [] as Set
+        result[5][1] = [1,5,6,7,9] as Set
         result[5][2] = [] as Set
-        result[5][3] = [] as Set
-        result[5][4] = [] as Set
-        result[5][5] = [] as Set
-        result[5][6] = [] as Set
-        result[5][7] = [] as Set
+        result[5][3] = [1,6,7,9] as Set
+        result[5][4] = [5,6,7,8,9] as Set
+        result[5][5] = [1,5,6,8,9] as Set
+        result[5][6] = [1,5,7,8] as Set
+        result[5][7] = [1,5,6,8,9] as Set
         result[5][8] = [] as Set
 
-        result[6][0] = [] as Set
-        result[6][1] = [] as Set
+        result[6][0] = [1,6,8,9] as Set
+        result[6][1] = [1,3,5,6,9] as Set
         result[6][2] = [] as Set
         result[6][3] = [] as Set
-        result[6][4] = [] as Set
-        result[6][5] = [] as Set
+        result[6][4] = [3,5,6,8,9] as Set
+        result[6][5] = [1,3,5,6,8,9] as Set
         result[6][6] = [] as Set
-        result[6][7] = [] as Set
-        result[6][8] = [] as Set
+        result[6][7] = [1,3,5,6,8,9] as Set
+        result[6][8] = [3,5,8,9] as Set
 
         result[7][0] = [] as Set
         result[7][1] = [] as Set
-        result[7][2] = [] as Set
-        result[7][3] = [] as Set
+        result[7][2] = [6] as Set
+        result[7][3] = [1,3,6,7] as Set
         result[7][4] = [] as Set
-        result[7][5] = [] as Set
+        result[7][5] = [1,3,6] as Set
         result[7][6] = [] as Set
-        result[7][7] = [] as Set
-        result[7][8] = [] as Set
+        result[7][7] = [1,3,4,6] as Set
+        result[7][8] = [3,7] as Set
 
-        result[8][0] = [] as Set
-        result[8][1] = [] as Set
-        result[8][2] = [] as Set
+        result[8][0] = [1,2,6,7,9] as Set
+        result[8][1] = [1,2,4,5,6,7,9] as Set
+        result[8][2] = [5,6,9] as Set
         result[8][3] = [] as Set
-        result[8][4] = [] as Set
-        result[8][5] = [] as Set
+        result[8][4] = [4,5,6,7,9] as Set
+        result[8][5] = [1,5,6,9] as Set
         result[8][6] = [] as Set
-        result[8][7] = [] as Set
-        result[8][8] = [] as Set
+        result[8][7] = [1,2,4,5,6,9] as Set
+        result[8][8] = [2,5,7,9] as Set
 
         SudokuSlotArray[] rows = testBoard1.getBoardRows()
-        SudokuSlotArray[] columns = testBoard1.getBoardColumns()
         rows.each { row ->
             SudokuSlot[] slots = row.getSquares()
-            slots.each { slot ->
-                if(slot.hasValue()) {
-                    assert slot.possibleValues.empty
-                } else {
-                    assert slot.possibleValues == [1,2,3,4,5,6,7,8,9] as Set
-                }
-            }
-        }
-
-        columns.each { column ->
-            SudokuSlot[] slots = column.getSquares()
             slots.each { slot ->
                 if(slot.hasValue()) {
                     assert slot.possibleValues.empty
@@ -203,51 +203,72 @@ class SudokuBoardTest extends GroovyTestCase {
 
         rows.eachWithIndex { row, rowIndex ->
             SudokuSlot[] slots = row.getSquares()
-            slots.each { slot ->
-                if(slot.hasValue()) {
-                    assert slot.possibleValues.empty
-                } else if(rowIndex == 0){
-                    if(slot.columnIndex == 0) {
-                        assert slot.possibleValues == [] as Set
-                    } ellse
-                } else if(rowIndex == 1){
-                    assert slot.possibleValues == [] as Set
-                } else if(rowIndex == 2){
-                    assert slot.possibleValues == [] as Set
-                } else if(rowIndex == 3){
-                    assert slot.possibleValues == [] as Set
-                } else if(rowIndex == 4){
-                    assert slot.possibleValues == [] as Set
-                } else if(rowIndex == 5){
-                    assert slot.possibleValues == [] as Set
-                } else if(rowIndex == 6){
-                    assert slot.possibleValues == [] as Set
-                } else if(rowIndex == 7){
-                    assert slot.possibleValues == [] as Set
-                } else if(rowIndex == 8){
-                    assert slot.possibleValues == [] as Set
-                }
+            slots.eachWithIndex { slot, columnIndex ->
+                assert slot.possibleValues == result[rowIndex][columnIndex]
             }
         }
     }
 
+    //TODO
     void testAssignCorrectValues() {
 
     }
 
     void testCalculatePossibleValueOccurrences() {
+        Map[] rowPossibleValueOccurrences = new Map[9]
+        rowPossibleValueOccurrences[0] = [1:2, 4:1, 5:5, 6:4, 7:2, 8:5, 9:6]
+        rowPossibleValueOccurrences[1] = [2:2, 3:2, 5:3, 8:3, 9:4]
+        rowPossibleValueOccurrences[2] = [2:4, 3:4, 5:4, 6:4, 8:4, 9:6]
+        rowPossibleValueOccurrences[3] = [2:2, 3:4, 5:3, 7:3, 8:4, 9:6]
+        rowPossibleValueOccurrences[4] = [1:2, 2:2, 3:3, 6:4, 7:5, 8:6, 9:6]
+        rowPossibleValueOccurrences[5] = [1:3, 5:3, 6:4, 7:4, 8:4, 9:5]
+        rowPossibleValueOccurrences[6] = [1:4, 3:3, 5:4, 6:5, 8:2, 9:4]
+        rowPossibleValueOccurrences[7] = [1:3, 3:2, 4:1, 6:4, 7:2]
+        rowPossibleValueOccurrences[8] = [1:4, 2:2, 4:2, 5:4, 6:6, 7:2, 9:5]
 
+
+        Map[] columnPossibleValueOccurrences = new Map[9]
+        columnPossibleValueOccurrences[0] = [1:3, 2:3, 6:5, 7:2, 8:4, 9:6]
+        columnPossibleValueOccurrences[1] = [1:4, 2:3, 3:1, 4:1, 5:5, 6:6, 7:4, 9:8]
+        columnPossibleValueOccurrences[2] = [5:1, 6:3, 8:2, 9:3]
+        columnPossibleValueOccurrences[3] = [1:2, 2:1, 3:4, 6:4, 7:3, 9:5]
+        columnPossibleValueOccurrences[4] = [3:4, 4:1, 5:4, 6:6, 7:4, 8:5, 9:7]
+        columnPossibleValueOccurrences[5] = [1:4, 3:4, 5:3, 6:4, 8:3, 9:5]
+        columnPossibleValueOccurrences[6] = [1:1, 5:3, 7:2, 8:4]
+        columnPossibleValueOccurrences[7] = [1:4, 2:3, 3:3, 4:2, 5:6, 6:3, 8:6, 9:5]
+        columnPossibleValueOccurrences[8] = [2:2, 3:2, 5:4, 7:3, 8:4, 9:3]
+
+        Map[][] blockPossibleValueOccurrences = new Map[3][3]
+        blockPossibleValueOccurrences[0][0] = [2:4, 5:3, 6:4, 7:2, 8:3, 9:6]
+        blockPossibleValueOccurrences[0][1] = [3:4, 4:1, 5:3, 6:4, 8:3, 9:6]
+        blockPossibleValueOccurrences[0][2] = [1:2, 2:2, 3:2, 5:6, 8:6, 9:4]
+        blockPossibleValueOccurrences[1][0] = [1:3, 5:3, 6:4, 7:4, 8:3, 9:6]
+        blockPossibleValueOccurrences[1][1] = [1:2, 2:1, 3:4, 6:4, 7:5, 8:5, 9:7]
+        blockPossibleValueOccurrences[1][2] = [2:3, 3:3, 5:3, 7:3, 8:6, 9:4]
+        blockPossibleValueOccurrences[2][0] = [1:4, 2:2, 3:1, 4:1, 6:6, 9:5]
+        blockPossibleValueOccurrences[2][1] = [1:4, 3:4, 5:4, 6:6, 7:2, 9:4]
+        blockPossibleValueOccurrences[2][2] = [1:3, 4:2, 5:4, 6:3, 7:2, 8:2]
+
+        testBoard1.calculatePossibleValuesByRowsAndColumns()
+        testBoard1.calculatePossibleValuesByBlocks()
+        testBoard1.calculatePossibleValueOccurrences()
+
+        SudokuBlock[][] boardBlocks = testBoard1.getBoardBlocks()
+        SudokuSlotArray[] boardColumns = testBoard1.getBoardColumns()
+        SudokuSlotArray[] boardRows = testBoard1.getBoardRows()
+
+        boardRows.eachWithIndex{ row, rowIndex ->
+            assert row.getPossibleValueOccurrences() == rowPossibleValueOccurrences[rowIndex]
+        }
+
+        boardColumns.eachWithIndex{ column, columnIndex ->
+            assert column.getPossibleValueOccurrences() == columnPossibleValueOccurrences[columnIndex]
+        }
+
+        boardBlocks.eachWithIndex{ blockRow, rowIndex ->
+            blockRow.eachWithIndex { block, columnIndex ->
+                assert block.getPossibleValueOccurrences() == blockPossibleValueOccurrences[rowIndex][columnIndex]
+            }
+        }
     }
-
-//    void testPrintBoard() {
-//
-//    }
-//
-//    void testPrintPossibleValues() {
-//
-//    }
-//
-//    void testPrintPossibleValueOccurrences() {
-//
-//    }
 }
